bc4af0892dab2bfc6a8232f7f01ea0e71c100717
Tonmoy-Saha-BS23 <tonmoy.saha@brainstation-23.com>
2023-04-04 12:59:06 +0600

    Code added for one-to-one(Bi-directional) connection between instructor and instructor details

---
+++ src/com/bs23/hibernate/demo/OneToOneBiDirectionalDemo.java
@@ -0,0 +1,47 @@
+package com.bs23.hibernate.demo;
+
+import org.hibernate.cfg.Configuration;
+
+import com.bs23.hibernate.demo.entity.Instructor;
+import com.bs23.hibernate.demo.entity.InstructorDetails;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.*;
+
+
+public class OneToOneBiDirectionalDemo {
+	public static void main(String[] args) {
+		// Create a session Factory
+		SessionFactory sessionFactory = new Configuration()
+										.configure("hibernate.cfg.xml")
+										.addAnnotatedClass(InstructorDetails.class)
+										.addAnnotatedClass(Instructor.class)
+										.buildSessionFactory();
+		//create session
+		Session session = sessionFactory.getCurrentSession();
+		
+		try {
+			// start the transaction
+			session.beginTransaction();
+			int insId = 2;
+			
+			// getting the instructor
+			InstructorDetails tempDetails = session.get(InstructorDetails.class, insId);
+			
+			System.out.println("Instructor details info: " + tempDetails);
+			
+			if(tempDetails != null) {
+				System.out.println("getting the instructor info");
+				Instructor tempInstructor = tempDetails.getInstructor();
+				System.out.println("Instructor Details: " + tempInstructor);
+			}
+			session.getTransaction().commit();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+	}
+}
